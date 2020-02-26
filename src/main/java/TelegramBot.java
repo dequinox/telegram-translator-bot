4d5ed3b761da6e58c8cf8bb7bed215196990f663
dequinox <umarov.doro@gmail.com>
2020-02-26 13:01:54 +0300

    Add Telegram Bot and Credentials

--- src/main/java/TelegramBot.java
+++ src/main/java/TelegramBot.java
@@ -1,2 +1,16 @@
-public class TelegramBot {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class TelegramBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+        System.out.println(update.getMessage().getText());
+    }
+
+    public String getBotUsername() {
+        return "dev_translator_bot";
+    }
+
+    public String getBotToken() {
+        return "1012476690:AAFwQ5coMPAuTOgd-07cbNXW1SndLbrDndU";
+    }
 }
