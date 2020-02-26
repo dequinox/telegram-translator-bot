0d137e96f66f88916b96a85dddbc7f58c3af7b1b
dequinox <umarov.doro@gmail.com>
2020-02-26 14:03:30 +0300

    Add functionality to TelegramBot to detect language from text and translate it into either russian or english

--- src/main/java/TelegramBot.java
+++ src/main/java/TelegramBot.java
@@ -1,9 +1,65 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.io.IOException;
+import java.util.Map;
 
 public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         System.out.println(update.getMessage().getText());
+
+        Map<String, String> langs = null;
+        try {
+            langs = TranslatorAPI.getLangs();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String input = update.getMessage().getText();
+
+        String source = TranslatorAPI.getKey(langs, "russian");
+        try {
+            source = TranslatorAPI.detectLanguage(input);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String target = TranslatorAPI.getKey(langs, "english");
+        if (langs.get(source) == langs.get(target))
+        {
+            target = TranslatorAPI.getKey(langs, "russian");
+        }
+
+        String output = null;
+        try {
+            output = TranslatorAPI.translate(input, source, target);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Detected lang: " + source + " (" + langs.get(source) + ")");
+
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + output);
+
+        System.out.println("Source: " + source);
+        System.out.println("Target: " + target);
+
+        System.out.println("langs.get(source): " + langs.get(source));
+        System.out.println("langs.get(target): " + langs.get(target));
+        System.out.println("TranslateAPI.getKey(langs, \"english\"): " + TranslatorAPI.getKey(langs, "english"));
+
+        SendMessage message = new SendMessage();
+
+        message.setText(output);
+        message.setChatId(update.getMessage().getChatId());
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 
     public String getBotUsername() {
