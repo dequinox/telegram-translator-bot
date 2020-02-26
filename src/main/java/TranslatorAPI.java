d6824bbea0f81a221ada9802d1873730faa2f8c1
dequinox <umarov.doro@gmail.com>
2020-02-26 13:21:25 +0300

    Add request function to retrieve information in TranslatorApi

--- src/main/java/TranslatorAPI.java
+++ src/main/java/TranslatorAPI.java
@@ -1,2 +1,25 @@
+import java.io.*;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TranslatorAPI {
+    public static final String API_KEY = "trnsl.1.1.20200226T160535Z.020875383576e81c.6dd6494b3008021c5beb59f70490c7ecc320acf5";
+
+    private static String request(String URL) throws IOException {
+        java.net.URL url = new URL(URL);
+        URLConnection urlConn = url.openConnection();
+        urlConn.addRequestProperty("User-Agent", "Mozilla");
+
+        InputStream inStream = urlConn.getInputStream();
+
+        String recieved = new BufferedReader(new InputStreamReader(inStream)).readLine();
+
+        inStream.close();
+        return recieved;
+    }
+
 }
